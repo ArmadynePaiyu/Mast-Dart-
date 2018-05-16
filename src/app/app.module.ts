2ae92fb3f9a6595fbcca8b2955c80c0f974e55f7
mani-bharathi-aruchamy <mani-bharathi.aruchamy@hpe.com>
2018-05-16 15:06:10 +0530

    Deleting unwanted components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,13 @@ import { RouterModule } from '@angular/router';
 import { AngularSlickgridModule } from 'angular-slickgrid';
 import * as $ from 'jquery';
 import { AppComponent } from './app.component';
-// import { FilterComponentComponent } from './filter-component/filter-component.component';
-// import { ExcelComponent } from './excel/excel.component';
 import { Grid1Component } from './grid1/grid1.component';
 import 'slickgrid';
 import 'jquery-ui';
-import { MenuComponent } from './menu/menu.component';
 import { SectionComponent } from './section/section.component';
 import { MarginSummaryComponent } from './section/margin-summary/margin-summary.component';
 import { MarginDetailComponent } from './section/margin-detail/margin-detail.component';
-import { DealViewComponent } from './section/deal-view/deal-view.component';
-import { DealInfoComponent } from './section/deal-view/deal-info/deal-info.component';
-import { UserregistrationComponent } from './userregistration/userregistration.component';
-import { ConfirmComponent } from './userregistration/confirm/confirm.component';
-import { SelfregistrationComponent } from './userregistration/selfregistration/selfregistration.component';
-import { NewuserregistrationComponent } from './userregistration/newuserregistration/newuserregistration.component';
-import { CountryPlselectionComponent } from './userregistration/country-plselection/country-plselection.component';
+import { DealInfoComponent } from './section/deal-info/deal-info.component';
 import { SidenavComponent } from './section/sidenav/sidenav.component';
 
 
@@ -27,23 +18,16 @@ import { SidenavComponent } from './section/sidenav/sidenav.component';
   declarations: [
     AppComponent,
     Grid1Component,
-    MenuComponent,
     SectionComponent,
     MarginSummaryComponent,
     MarginDetailComponent,
-    DealViewComponent,
     DealInfoComponent,
-    UserregistrationComponent,
-    ConfirmComponent,
-    SelfregistrationComponent,
-    NewuserregistrationComponent,
-    CountryPlselectionComponent,
     SidenavComponent
   ],
   imports: [
     BrowserModule,
     AngularSlickgridModule.forRoot(),
-    RouterModule.forRoot([
+    /* RouterModule.forRoot([
       {
         path: 'selfReg',
         component: SelfregistrationComponent
@@ -68,7 +52,7 @@ import { SidenavComponent } from './section/sidenav/sidenav.component';
         path: 'confirm',
         component: ConfirmComponent
       }
-    ])
+    ]) */
   ],
   providers: [],
   bootstrap: [AppComponent]
